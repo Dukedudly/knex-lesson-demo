bb2912616d7ad21e76146a77767d34307ca758e9
Ty Hancock <33936847+Dukedudly@users.noreply.github.com>
2023-03-21 16:22:05 -0400

    final update.
    Now able to successfully run an Express server and access ./pets to display pet names! I did it!

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 5432;
+const port = 5433;
+const knex = require('knex')(require('../knexfile.js')["development"])
 
+app.get('/', (req,res) => {
+  res.send('App is up and running.')
+})
+
+
+app.listen(port, (err) => {
+  if (err) {throw err} else{
+  console.log('your app is compatible with express and knex! Yay you did it!')
+}})
+
+app.get('/pets', (req,res) =>{
+  knex('pet')
+    .select('*')
+    .then(pets => {
+      var petNames = pets.map(pet => pet.name)
+      res.json(petNames)
+    })
+})
